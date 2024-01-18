9255f64a81857df39bb8d7392d1fc232e6c93cd3
NgDK27 <85181668+NgDK27@users.noreply.github.com>
2024-01-18 22:30:52 +0700

    Final notebook version

--- inference.py
+++ inference.py
@@ -1,41 +1,55 @@
-# import subprocess
-# import sys
+import subprocess
+import sys
 
-# def install(package):
-#     subprocess.check_call([sys.executable, "-m", "pip", "install", package])
+def install(package):
+    subprocess.check_call([sys.executable, "-m", "pip", "install", package])
 
-# try:
-#     import joblib
-# except ImportError:
-#     install('joblib')  # specify the version you need
-# import joblib
-# import numpy as np
+try:
+    import joblib
+except ImportError:
+    install('joblib')  # specify the version you need
+import joblib
+import numpy as np
+import logging
 
-# def model_fn(model_dir):
-#     model = joblib.load(f"{model_dir}/model.joblib")
-#     return model
+# Set up logging
+logging.basicConfig(level=logging.DEBUG)
 
+def model_fn(model_dir):
+    """
+    Load the joblib model from the specified directory.
+    """
+    try:
+        model = joblib.load(f"{model_dir}/model.joblib")
+        logging.info("Model loaded successfully.")
+        return model
+    except Exception as e:
+        logging.error("Error in loading model: %s", e)
+        raise
 
-# def input_fn(request_body, request_content_type):
-#     if request_content_type == "text/csv":
-#         # Convert string to numpy array
-#         print("here")
-#         data = np.fromstring(request_body, sep=',')
+def input_fn(request_body, request_content_type):
+    """
+    Parse input data payload
+    """
+    try:
+        if request_content_type == "text/csv":
+            data = np.genfromtxt(request_body.splitlines(), delimiter=',')
+            logging.info("Input data processed successfully.")
+            return data.reshape(1, -1)  # Reshape if necessary
+        else:
+            raise ValueError(f"Unsupported content type: {request_content_type}")
+    except Exception as e:
+        logging.error("Error in input_fn: %s", e)
+        raise
 
-#         # Print the original shape
-#         print("Original shape:", data.shape)
-
-#         # Reshape the data
-#         data = data.reshape(1, -1)
-
-#         # Print the new shape
-#         print("New shape:", data.shape)
-
-#         return data
-#     else:
-#         # Handle other content-types or raise an exception
-#         pass
-
-# def predict_fn(input_data, model):
-#     prediction = model.predict(input_data)
-#     return prediction
+def predict_fn(input_data, model):
+    """
+    Make a prediction using the input data and loaded model.
+    """
+    try:
+        predictions = model.predict(input_data)
+        logging.info("Prediction successful.")
+        return predictions
+    except Exception as e:
+        logging.error("Error in predict_fn: %s", e)
+        raise
